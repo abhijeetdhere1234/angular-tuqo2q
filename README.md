d382e9414332d8fb6d31546ea1b775ef9c014285
abhijeetdhere1234 <abhijeet100712@gmail.com>
2019-05-14 14:24:47 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-tuqo2q
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-tuqo2q)
---
+++ src/app/employee/employee.component.ts
@@ -0,0 +1,20 @@
+import {Component} from "@angular/core"
+
+@Component(
+  {
+    selector:"my-employee",
+    templateUrl:"./employee.component.html"
+  }
+)
+
+export class EmployeeComponent{
+  firstName : string ="Tom";
+  lastName: string = "Hopkins";
+  gender: string="Male";
+  age:number =18;
+  showDetails: boolean = false;
+
+  toggleDetails() :void{
+    this.showDetails =!this.showDetails;
+  }
+}
