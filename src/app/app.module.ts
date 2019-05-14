eca8d04d4b996229c7baf3e0b790b89063c21fd3
abhijeetdhere1234 <abhijeet100712@gmail.com>
2019-05-14 18:20:00 +0530

    2 files modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,12 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { EmployeeComponent } from './employee/employee.component';
+import { EmployeeListComponent } from './employee/employeeList.component';
 
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent,EmployeeComponent ],
+  declarations: [ AppComponent, HelloComponent,EmployeeComponent,EmployeeListComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/employee/employeeList.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core'
+
+@Component(
+  {
+    selector:'employee-list',
+    templateUrl:'/employeeList.component.html'
+  }
+)
+
+export class EmployeeListComponent{
+
+}
