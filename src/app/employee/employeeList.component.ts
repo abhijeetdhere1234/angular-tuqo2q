64eedbbc7217b2079a7f79753df190b81d9a52b1
abhijeetdhere1234 <abhijeet100712@gmail.com>
2019-05-14 18:30:07 +0530

    2 files modified, 1 added

--- src/app/employee/employeeList.component.ts
+++ src/app/employee/employeeList.component.ts
@@ -3,10 +3,14 @@ import {Component} from '@angular/core'
 @Component(
   {
     selector:'employee-list',
-    templateUrl:'/employeeList.component.html'
+    templateUrl:'/employeeList.component.html',
+    styleUrls: ['/employeeList.component.css']
   }
 )
 
 export class EmployeeListComponent{
-
+employees : any[]=[
+  {code:'1', name:'a',annualSalary: 500,gender:'Male',dateofBirth:'5/9/2010'},
+  {code:'2', name:'b',annualSalary: 500,gender:'Male',dateofBirth:'5/9/2010'}
+];
 }
