c58bfc48d5333246f8f4aa9f6153a0f170d59a25
abhijeetdhere1234 <abhijeet100712@gmail.com>
2019-05-15 13:52:36 +0530

    2 files modified

--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -8,13 +8,31 @@ import {Component} from "@angular/core"
 )
 
 export class EmployeeComponent{
-  firstName : string ="Tom";
-  lastName: string = "Hopkins";
-  gender: string="Male";
-  age:number =18;
-  showDetails: boolean = false;
+  // firstName : string ="Tom";
+  // lastName: string = "Hopkins";
+  // gender: string="Male";
+  // age:number =18;
+  // showDetails: boolean = false;
 
-  toggleDetails() :void{
-    this.showDetails =!this.showDetails;
-  }
+  // toggleDetails() :void{
+  //   this.showDetails =!this.showDetails;
+  // }
+  employees : any[];
+  constructor(){
+  this.employees =[
+  {code:'1', name:'a',annualSalary: 500,gender:'Male',dateofBirth:'5/9/2010'},
+  {code:'2', name:'b',annualSalary: 500,gender:'Female',dateofBirth:'5/9/2010'}
+];}
+
+getEmployees() : void{
+ this.employees =[
+  {code:'1', name:'a',annualSalary: 500,gender:'Male',dateofBirth:'5/9/2010'},
+  {code:'2', name:'b',annualSalary: 500,gender:'Female',dateofBirth:'5/9/2010'},
+  {code:'3', name:'c',annualSalary: 500,gender:'Male',dateofBirth:'5/9/2000'}
+];
+}
+
+trackByEmpCode(index:number,employee:any):string{
+return employee.code;
+}
 }
